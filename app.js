7f24550fb72a89d37933af7272aba473c626df89
Rahim Pormah <sas4n.porm4h@gmail.com>
2022-08-05 02:17:16 +0200

    mongoose configured

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const mongoose = require('mongoose')
 const path = require('path')
 const hbs = require('express-hbs')
 const morgan = require('morgan')
-const { appendFile } = require('fs')
 
 const app = express()
+
+const yourMongoDbURIGoesHere = ''
+const mongoDb_URI = process.env.MONGODB_URI || yourMongoDbURIGoesHere
+
+mongoose.connect(mongoDb_URI, { useNewUrlParser: true,useUnifiedTopology: true})
+    .then(app.listen(5000, () => {console.log('server is running on port 5000')}))
+    .catch(err => {console.log(err)})
 
 // Use `.hbs` for extensions and find partials in `views/partials`.
 app.engine('hbs', hbs.express4({
@@ -22,4 +28,4 @@ app.use(express.urlencoded({ extended : false}))
 
 app.use('/', require('./routes/snippetRouter'))
 
-app.listen(5000 , () => {console.log('server running on port 5000')})
+
